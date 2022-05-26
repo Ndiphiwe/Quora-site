5d608b97ef7a75f23c9f667edb0c177a2b6403a2
Ndiphiwe <pndira1770@gmail.com>
2022-05-26 14:48:14 +0200

    did a few changes

---
+++ assets/js/rollover.js
@@ -0,0 +1,15 @@
+//Image RollOver for arrow slider
+function chngPrevArrow() {
+  document.getElementById("prevArrow").src =
+    "/assets/images/icons/prev-fill.png";
+}
+function chngPrevArrowBack() {
+  document.getElementById("prevArrow").src = "/assets/images/icons/prev.png";
+}
+function chngNextArrow() {
+  document.getElementById("nextArrow").src =
+    "/assets/images/icons/next-fill.png";
+}
+function chngNextArrowBack() {
+  document.getElementById("nextArrow").src = "/assets/images/icons/next.png";
+}
